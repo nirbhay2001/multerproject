b285fdf45c93fe90748d114c6b04ca032716a98e
nirbhay2001 <nirbhayggupta2001@gmail.com>
2024-08-21 12:24:54 +0530

    add cloud backend url in frontend

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4004",
+  baseURL: "https://multerproject.onrender.com",
   headers: {
     "Content-Type": "application/json",
     "Content-Type":"multipart/form-data"
